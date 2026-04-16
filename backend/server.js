4ea7832cde7cd47a0b9660a138fd12d5a82f96c2
Pavel Glebik <g1ebikpasha@gmail.com>
2026-04-16 17:40:05 +0300

    backend updates 2
    Signed-off-by: Pavel Glebik <g1ebikpasha@gmail.com>

--- backend/server.js
+++ backend/server.js
@@ -64,5 +64,5 @@ app.use((err, req, res, _next) => {
 app.listen(PORT, () => {
   console.log(`🚀 Сервер запущен на порту ${PORT}`);
   console.log(`📊 API доступен по адресу: http://localhost:${PORT}/api`);
-  console.log('📊 API Version: 1.0.1');
+  console.log('📊 API Version: 1.0.2');
 });
